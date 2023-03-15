42e88d6380a028253b0e5bf03db4e913312bf8e0
Wes <wesleyyw8@gmail.com>
2023-03-15 11:29:02 -0300

    tests regex and implementation of valid expression without sen cos tg done

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -6,18 +6,72 @@ describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+  // beforeEach(async () => {
+  //   await TestBed.configureTestingModule({
+  //     declarations: [ HomeComponent ]
+  //   })
+  //   .compileComponents();
 
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  //   fixture = TestBed.createComponent(HomeComponent);
+  //   component = fixture.componentInstance;
+  //   fixture.detectChanges();
+  // });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+
+  describe('it should test the private functions', () => {
+
+    it('should be a valid math expressions 1', () => {
+      // @ts-expect-error
+      expect(new HomeComponent().isValidMathExpression('3 + 2 + 4')).toBeTrue();
+    });
+    it('should be a valid math expressions 2', () => {
+      // @ts-expect-error
+      expect(new HomeComponent().isValidMathExpression('+2')).toBeTrue();
+    });
+    
+    it('should be a valid math expressions 3', () => {
+      // @ts-expect-error
+      expect(new HomeComponent().isValidMathExpression('-2')).toBeTrue();
+    });
+
+    xit('should be a INVALID math expressions 4', () => {
+      // @ts-expect-error
+      expect(new HomeComponent().isValidMathExpression('3 + +')).toBeFalse();
+    });
+
+    xit('should be a INVALID math expressions 5', () => {
+      // @ts-expect-error
+      expect(new HomeComponent().isValidMathExpression('3+')).toBeFalse();
+    });
+
+    
+    xit('should be a valid math expressions 4', () => {
+      // @ts-expect-error
+      expect(new HomeComponent().isValidMathExpression('sin(sin(30) + cos(20))')).toBeTrue();
+    });
+
+    xit('should be a valid math expressions', () => {
+      // @ts-expect-error
+      expect(new HomeComponent().isValidMathExpression('sin(30) + cos(20)')).toBeTrue();
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+    // it('should be a invalid math expressions', () => {
+    //   // @ts-expect-error
+    //   expect(new HomeComponent().isValidMathExpression('sin(30')).toBeTrue();
+    // });
+  })
 });
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  
+  private isValidMathExpression(expr: string): boolean {
+    //this regex will solve expressions like "3 + 2 + 4", "+3 + 2 + 4", "2 + 4", but not match expressions like "+3 + 2 + 4 +" or "3 + 2 + 4 +".
+    const numberRegex = /^\s*(\+|-)?\d+(\s*[+*\/-]\s*(\+|-)?\d+)*\s*$/;
 
+    if (numberRegex.test(expr)) {
+      return true;
+    }
+    return false;
+    // const senCosTgRegex = 
+    // return numberRegex.test(expr);
+  }
 }
